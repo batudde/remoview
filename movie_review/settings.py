d80214847e498d2f8ba77eea29f8b4dd1a00ac6c
batudde <batuhandede17@gmail.com>
2025-05-03 23:37:21 +0300

    Added Render domain to ALLOWED_HOSTS

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-ibwrodj&czpof)3xa7jng6b_zjbo3mwg=h@58u_cn-=&n&2k_q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['remoview.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
